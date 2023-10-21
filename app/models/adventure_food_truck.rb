7564711da9955551b7b3bce560e1c7515f06f418
Cody Goeken <codygoeken@Codys-MacBook-Pro-2.local>
2023-10-21 16:02:16 -0700

    created tests for join model

--- app/models/adventure_food_truck.rb
+++ app/models/adventure_food_truck.rb
@@ -3,4 +3,8 @@ class AdventureFoodTruck < ApplicationRecord
   belongs_to :food_truck
 
   validates :order, uniqueness: { scope: :adventure_id }
+  validates :order, numericality: { only_integer: true }
+  validates :adventure, :food_truck, presence: true
+
+  scope :ordered, -> { order(:order) }
 end
--- spec/models/adventure_food_truck_spec.rb
+++ spec/models/adventure_food_truck_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe AdventureFoodTruck, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:adventure) }
+    it { should belong_to(:food_truck) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:adventure) }
+    it { should validate_presence_of(:food_truck) }
+    it { should validate_numericality_of(:order).only_integer }
+  end
+
+  describe 'order scope' do
+    let!(:adventure) { create(:adventure) }
+    let!(:food_truck_1) { create(:food_truck) }
+    let!(:food_truck_2) { create(:food_truck) }
+    let!(:aft_1) { AdventureFoodTruck.create(adventure:, food_truck: food_truck_1, order: 2) }
+    let!(:aft_2) { AdventureFoodTruck.create(adventure:, food_truck: food_truck_2, order: 1) }
+
+    it 'returns adventure_food_trucks in order' do
+      expect(AdventureFoodTruck.ordered).to eq([aft_2, aft_1])
+    end
+  end
 end
