7f2c2bcdcde2218b03d662f94bf798f99535d8ee
Cody Goeken <codygoeken@Codys-MacBook-Pro-2.local>
2023-10-24 13:39:14 -0700

    secure auth for twilio

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -2,11 +2,12 @@
 
 class SmsController < ApplicationController
   skip_before_action :verify_authenticity_token  # Skip CSRF check for webhook requests
+  before_action :validate_twilio_request, only: [:receive]
 
   def receive
     body = params['Body'].strip.downcase
     phone_number = params['From']
-    adventure = Adventure.where(phone_number:).order(created_at: :desc).first
+    adventure = Adventure.where(phone_number: phone_number).order(created_at: :desc).first
 
     if adventure.nil?
       SmsService.send_sms(
@@ -18,5 +19,17 @@ class SmsController < ApplicationController
 
     response = SmsService.process_command(adventure, body)
     SmsService.send_sms(phone_number, response[:message])
+  end
+
+  private
+
+  def validate_twilio_request
+    twilio_signature = request.headers['HTTP_X_TWILIO_SIGNATURE']
+  
+    validator = Twilio::Security::RequestValidator.new(Rails.application.credentials.dig(:twilio, :auth_token))
+  
+    unless validator.validate(request.original_url, request.POST, twilio_signature)
+      render plain: 'Unauthorized request', status: :unauthorized
+    end
   end
 end
